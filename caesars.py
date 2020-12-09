9cf7056ee8feb752d60ec9361689d723ff5e69bf
jvhuang1786 <jvhuang@hotmail.com>
2020-12-09 04:26:13 -0800

    update

--- caesars.py
+++ caesars.py
@@ -92,7 +92,7 @@ def main():
  'B170506133','B170506134','MRXU005523','MRXU005524','MRXU005525','MRXU005526','MRXU005527','MRXU005528'))
         st.write('You selected:', option)
         df_comb = pd.concat([df_post[df_post['c_serial_num']==option],
-        df_pre[df_pre['c_serial_num']==option]))
+        df_pre[df_pre['c_serial_num']==option])
         st.write(df_comb.T)
         # print(df_pre[option].agg('mtr_win over dof').mean())
         #
