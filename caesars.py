c48cc416ada19da070300029c0a045d0881fe029
jvhuang1786 <jvhuang@hotmail.com>
2020-12-09 02:48:41 -0800

    update

--- caesars.py
+++ caesars.py
@@ -32,6 +32,9 @@ def main():
         df_tables = df1.groupby('d_game_date').agg({'c_table_num': ['count'],'f_actual_win': ['sum', 'min', 'max', 'mean', 'median', 'std'],
                                 'Drop': ['sum', 'min', 'max', 'mean', 'median', 'std'],
                                          'actul win over minutes played': ['sum', 'min', 'max', 'mean', 'median', 'std']})
+
+        df_tables = df_tables.reset_index()
+        df_tables.d_game_date = df_tables.d_game_date.astype('str').apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
         #####
         start_date = st.sidebar.date_input('Start date', datetime.strptime('2020-10-01', '%Y-%m-%d'))
         end_date = st.sidebar.date_input('End date', datetime.strptime('2020-11-21', '%Y-%m-%d'))
