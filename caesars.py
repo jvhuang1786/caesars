dc6421e44bda3e0c25ad5be96d4e2f25a0bf3b56
jvhuang1786 <jvhuang@hotmail.com>
2020-12-09 05:02:40 -0800

    update

--- caesars.py
+++ caesars.py
@@ -80,10 +80,41 @@ def main():
         #####dataframes
         df_pre = pd.read_csv('df_pre.csv',index_col = 0)
         df_post = pd.read_csv('df_post.csv',index_col = 0)
+        df2_post_inplace = pd.read_csv('df_inplace_post.csv',index_col =0)
+        df2_pre_inplace = pd.read_csv('df_inplace_pre.csv',index_col=0)
         #####
         st.write('There were a total of 34 slot machines starting in zone 2. 32 slot machines were then moved from other zones to zone 2. Slot machines that moved are indicated by "moving" and those that stayed in place are indicated by "inplace" under the slot logistics column.')
         st.write('This is the pre_move table for the 32 slot machines and zone 2', df_pre)
         st.write('This is the post_move table for the 32 slot machines that moved into zone 2', df_post)
+
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            x=['2174121', '2174399', '2174131', '2174413', '2174339', '2174373',
+       '1949577', '2174166', '2174125', '2174367', '1964676', '1964674',
+       '1949572', '2174122', '2174123', '1965200', '1949828', '2174127',
+       '2174341', '2174128', '2174372', '1965309', '2174393', '1948962',
+       '2174358', '2174167', '2174120', '2174126', '2174124', '2174130',
+       '2174353', '2174332', '2174387', '2174129'],
+            y=df_comb[['mtr_win over dof', 'coin_in over dof']],
+            name='Pre Move',
+            marker_color='silver'
+        ))
+        fig.add_trace(go.Bar(
+            x=['2174121', '2174399', '2174131', '2174413', '2174339', '2174373',
+       '1949577', '2174166', '2174125', '2174367', '1964676', '1964674',
+       '1949572', '2174122', '2174123', '1965200', '1949828', '2174127',
+       '2174341', '2174128', '2174372', '1965309', '2174393', '1948962',
+       '2174358', '2174167', '2174120', '2174126', '2174124', '2174130',
+       '2174353', '2174332', '2174387', '2174129'],
+            y=df_comb[['mtr_win over dof', 'coin_in over dof']],
+            name='Post Move',
+            marker_color='gold'
+        ))
+
+        # Here we modify the tickangle of the xaxis, resulting in rotated labels.
+        fig.update_layout(title_text = 'Zone 2 Performance Post and Pre',barmode='group', xaxis_tickangle=-45)
+        st.plotly_chart(fig, use_container_width=True)
+
         option = st.selectbox(
         'Which of the 32 slot Machines do you want to compare?',
         ('2384807','2384808','2384809','2384810','2492692','2492693','2492696','2492697',
