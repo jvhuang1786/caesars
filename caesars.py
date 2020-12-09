b2231710e29ac067086242847c0fe2e2893065cf
jvhuang1786 <jvhuang@hotmail.com>
2020-12-09 03:44:48 -0800

    update

--- caesars.py
+++ caesars.py
@@ -40,17 +40,14 @@ def main():
         #mask = (df_tables['date'] <= start_date) & (df_tables['date'] >= end_date)
         st.write('Table count for date and Summary Statistics of Actual Win, Drop and Actual divided by Minutes Played',df_tables)
 
-        st.write('Open Table Count through the months of October to November')
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=df_tables.date, y=df_tables['table count'],
                             mode='lines',
                             line_color = 'light blue',
                             name='Number of Open Tables'))
-        fig.update_layout(title_text = 'Open Tables', xaxis_rangeslider_visible = True)
+        fig.update_layout(title_text = 'Open Table Count through the months of October to November', xaxis_rangeslider_visible = True)
 
         st.plotly_chart(fig, use_container_width=True)
-
-        st.write('Drop and Actual Wins')
 
         fig = go.Figure()
 
@@ -80,82 +77,9 @@ def main():
 #Question2
     if option == 'Slot Machines Question 2':
         st.title('Slot Machine Reallocation')
-
+        #####dataframes
+        #####
         st.write('blah blah blah')
 
 if __name__ == '__main__':
     main()
-# This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
-# * **Python libraries:** base64, pandas, streamlit, yfinance, numpy, matplotlib
-# * **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
-# """)
-
-
-
-# st.sidebar.header('User Input Features')
-#
-# # Web scraping of S&P 500 data
-# #
-# @st.cache
-# def load_data():
-#     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-#     html = pd.read_html(url, header = 0)
-#     df = html[0]
-#     return df
-#
-# df = load_data()
-# sector = df.groupby('GICS Sector')
-#
-# # Sidebar - Sector selection
-# sorted_sector_unique = sorted( df['GICS Sector'].unique() )
-# selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
-#
-# # Filtering data
-# df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
-#
-# st.header('Display Companies in Selected Sector')
-# st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
-# st.dataframe(df_selected_sector)
-#
-# # Download S&P500 data
-# # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-# def filedownload(df):
-#     csv = df.to_csv(index=False)
-#     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-#     href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
-#     return href
-#
-# st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
-#
-# # https://pypi.org/project/yfinance/
-#
-# data = yf.download(
-#         tickers = list(df_selected_sector[:10].Symbol),
-#         period = "ytd",
-#         interval = "1d",
-#         group_by = 'ticker',
-#         auto_adjust = True,
-#         prepost = True,
-#         threads = True,
-#         proxy = None
-#     )
-#
-# # Plot Closing Price of Query Symbol
-# def price_plot(symbol):
-#   df = pd.DataFrame(data[symbol].Close)
-#   df['Date'] = df.index
-#   fig = plt.figure()
-#   plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
-#   plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
-#   plt.xticks(rotation=90)
-#   plt.title(symbol, fontweight='bold')
-#   plt.xlabel('Date', fontweight='bold')
-#   plt.ylabel('Closing Price', fontweight='bold')
-#   return st.pyplot(fig)
-#
-# num_company = st.sidebar.slider('Number of Companies', 1, 5)
-#
-# if st.button('Show Plots'):
-#     st.header('Stock Closing Price')
-#     for i in list(df_selected_sector.Symbol)[:num_company]:
-#         price_plot(i)
