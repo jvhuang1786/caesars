de1e6a4641748045b0003f3db6c9675c5c8f5545
jvhuang1786 <jvhuang@hotmail.com>
2020-12-09 02:47:36 -0800

    update

--- caesars.py
+++ caesars.py
@@ -29,7 +29,9 @@ def main():
         ##dataframes##
         df1 = pd.read_json('df1.json')
         table_count = pd.read_json('table_count.json')
-        df_tables = pd.read_json('df_tables.json')
+        df_tables = df1.groupby('d_game_date').agg({'c_table_num': ['count'],'f_actual_win': ['sum', 'min', 'max', 'mean', 'median', 'std'],
+                                'Drop': ['sum', 'min', 'max', 'mean', 'median', 'std'],
+                                         'actul win over minutes played': ['sum', 'min', 'max', 'mean', 'median', 'std']})
         #####
         start_date = st.sidebar.date_input('Start date', datetime.strptime('2020-10-01', '%Y-%m-%d'))
         end_date = st.sidebar.date_input('End date', datetime.strptime('2020-11-21', '%Y-%m-%d'))
@@ -45,7 +47,7 @@ def main():
 
         st.write('This is the table number and their counts through the given dates.', table_count)
         st.write(df_tables)
-        mask = (df_tables['d_game_date'] >= start_date) & (df_tables['d_game_date'] <= end_date)
+        #mask = (df_tables['d_game_date'] >= start_date) & (df_tables['d_game_date'] <= end_date)
 
 
         st.write('blah blah blah')
